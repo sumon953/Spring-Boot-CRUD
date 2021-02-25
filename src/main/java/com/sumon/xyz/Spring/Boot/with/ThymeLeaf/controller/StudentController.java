f20d8dcb53c2fbf92c9bc1f29f30bd77c4c0c4bd
43031 <sumonsorder953@gmail.com>
2021-02-25 19:02:09 +0600

    Complete Form CRUD operation

--- src/main/java/com/sumon/xyz/Spring/Boot/with/ThymeLeaf/controller/StudentController.java
+++ src/main/java/com/sumon/xyz/Spring/Boot/with/ThymeLeaf/controller/StudentController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -20,25 +18,20 @@ public class StudentController {
     StudentRepository studentRepository;
 
 
-    @GetMapping("showForm")
-    public String showStudentForm()
-    {
+    @GetMapping("signup")
+    public String showSignUpForm(Student student) {
         return "add-student";
     }
 
 
-
     @GetMapping("list")
-    public String students(Model model)
-    {
-        model.addAttribute("student",this.studentRepository.findAll());
+    public String showUpdateForm(Model model) {
+        model.addAttribute("students", studentRepository.findAll());
         return "index";
     }
 
-
-
     @PostMapping("add")
-    public String addStudent(Student student, BindingResult result, Model model) {
+    public String addStudent(@Validated Student student, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-student";
         }
@@ -48,29 +41,35 @@ public class StudentController {
     }
 
 
-
     @GetMapping("edit/{id}")
-    public String showupdateform(){
-
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        model.addAttribute("student", student);
+        return "update-student";
     }
 
-
-
-
-    @GetMapping("update/{id}")
-    public String updateStudent(@PathVariable("id") Long id,Student student, BindingResult result,Model model)
-    {
-        if (result.hasErrors())
-        {
+    @PostMapping("update/{id}")
+    public String updateStudent(@PathVariable("id") long id, @Validated Student student, BindingResult result,
+                                Model model) {
+        if (result.hasErrors()) {
             student.setId(id);
             return "update-student";
         }
-        //Update Student;
-       studentRepository.save(student);
 
-        //Get All Student(with update)
-        model.addAttribute("student",studentRepository.findAll());
+        studentRepository.save(student);
+        model.addAttribute("students", studentRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteStudent(@PathVariable("id") long id, Model model) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+        studentRepository.delete(student);
+        model.addAttribute("students", studentRepository.findAll());
         return "index";
     }
 
 }
+
